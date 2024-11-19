fee5d8f47d267c70488ee156439dc5b96a37eeae
FebrinaAyuningrum <febrinamarhas@gmail.com>
2024-11-19 10:55:32 +0700

    psas mobile

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:psas/Home.dart';
+import 'Login page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplikasi Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
